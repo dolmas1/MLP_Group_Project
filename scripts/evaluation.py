a0218f5c7444bf6fc435dae55a7aba1415cdf0c8
zuefle <zuefle@cl.uni-heidelberg.de>
2023-03-02 21:12:47 +0000

    tiny error in evaluation

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -13,7 +13,7 @@ from prettytable import PrettyTable
 
 from integrated_gradients import get_integrated_gradients_score
 from attention import get_attention_scores
-from lime_shap import get_shap_scores
+from lime_shap import get_shap_scores, get_lime_scores
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 id2label = {0: "noHate", 1: "hate"}
@@ -31,7 +31,7 @@ def evaluate(model, test_loader, destination_path, model_name, tokenizer, model_
     """
     y_pred = []; y_true = []; y_probs = []
     batch_tokens = []
-    lig_scores = []; attention_scores = []; shap_scores = []
+    lig_scores = []; attention_scores = []; shap_scores = []; lime_scores = []
 
     model.eval()
     
@@ -55,6 +55,9 @@ def evaluate(model, test_loader, destination_path, model_name, tokenizer, model_
                 batch_tokens.append(toks)
                 tokens.append(toks)
 
+            # lime scores
+            lime_scores += get_lime_scores(model, text,tokenizer)
+
             # shap scores
             shap_scores += get_shap_scores(model, text,tokenizer)
             
@@ -77,8 +80,8 @@ def evaluate(model, test_loader, destination_path, model_name, tokenizer, model_
     y_true = np.array(y_true)
 
     result_table = PrettyTable(["Tokens", "Lime", "Shap", "Attention", "Integrated Gradients", "Probability for Hate", "Predicted Label"])
-    for tok, shap, att, lig, prob, pred in zip(batch_tokens, shap_scores, attention_scores, lig_scores, y_probs, y_pred):
-        result_table.add_row([tok, "", shap, att,  lig, round(prob, 2),  pred])
+    for tok, lime, shap, att, lig, prob, pred in zip(batch_tokens, lime_scores, shap_scores, attention_scores, lig_scores, y_probs, y_pred):
+        result_table.add_row([tok, lime, shap, att,  lig, round(prob, 2),  pred])
     result_table.border = False
     result_table.align = "l"
     with open(os.path.join(destination_path, f"predictions_model_{model_name}.csv"), "w+") as csv_out:
@@ -171,11 +174,6 @@ def evaluate_ensemble(constituent_models, constituent_model_names, test_loaders,
                     # layerwise integrated gradient
                     lig_scores += get_integrated_gradients_score(text, labels, tokens, tokenizer, model, model_type)
 
-                    # LIME
-                    ### ADD THIS HERE
-
-                    # SHAP
-                    ### ADD THIS HERE
 
                     y_probs.extend(probs.tolist())
                     y_pred.extend(torch.argmax(logits, 1).tolist())
