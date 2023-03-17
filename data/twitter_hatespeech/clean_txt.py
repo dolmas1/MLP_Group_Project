582aedd826b2d4ebeba48455e00bd867260c80ca
Sam Dolman <samueldolman@gmail.com>
2023-03-17 17:07:02 +0000

    clean up twitter data, add bootstrapped CIs to perf metrics

---
+++ data/twitter_hatespeech/clean_txt.py
@@ -0,0 +1,15 @@
+import re
+import pandas as pd
+
+df = pd.read_csv(f'../data/twitter_hatespeech/twitter_hatespeech.tsv', sep='\t')
+
+def clean_text(txt):
+    txt_clean = re.sub(r"\[NEWLINE\]", ' ', txt)                                  # remove NEWLINE
+    txt_clean = re.sub(r"https?:\/\/.*[\r\n]*", '', txt_clean)                    # strip URLs
+    txt_clean = re.sub(r"[^(\w|\s|(\'\,\-\_\.\%\!\$\&\(\)\;\:))]", '', txt_clean) # remove all other non-standard chars
+    txt_clean = re.sub(r"\s+", ' ', txt_clean)                                    # replace multiples spaces with single
+    
+    return txt_clean
+
+df.text = [clean_text(txt) for txt in df.text]
+df.to_csv(f'../data/twitter_hatespeech/twitter_hatespeech_clean.tsv', sep='\t')
